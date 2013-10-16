8ba334f4e64afc9139266a46f37f35fa6667bdb3
dennis <dennis@dennis-laptop>
2013-10-16 15:31:34 +0200

    add ini

--- module.php
+++ module.php
@@ -12,7 +12,7 @@ class disqus {
     <div id="disqus_thread"></div>
     <script type="text/javascript">
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'coscms'; // required: replace example with your forum shortname
+        var disqus_shortname = '$disqus_short_name'; // required: replace example with your forum shortname
 
         /* * * DON'T EDIT BELOW THIS LINE * * */
         (function() {
