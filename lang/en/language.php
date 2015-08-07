2b0297badea6506f03a4cb0d75c203a28a4c7d41
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:37:29 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,13 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/disqus/install.inc
+
+$LANG['Disqus'] = 'Disqus';
+// Translation of file modules/disqus/system_lang.inc
+
+$LANG['disqus_config_description'] = 'disqus_config_description';
+// Translation of file modules/disqus/configdb.inc
+
+$LANG['Enter your disqus code'] = 'Enter your disqus code';
