b349ce9c18f554a11b2a857ed6b2e025ec57b587
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-15 12:15:09 +0200

    refactor

--- config/module.php
+++ config/module.php
@@ -2,15 +2,22 @@
 
 namespace modules\disqus\config;
 
+use diversen\lang;
 use modules\configdb\module as configdb;
 
-
-
-
-
 class module {
-    public function indexAction () {
 
+    public function indexAction() {
         configdb::displayConfig('disqus');
+    }
+
+    public function getConfigSetup() {
+        return $db_config = array(
+            array('name' => 'disqus_short_name',
+                'description' => lang::translate('Enter your disqus code'),
+                'type' => 'text',
+                'value' => '',
+                'auth' => 'admin'),
+        );
     }
 }
