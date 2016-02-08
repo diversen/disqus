61eb750893c6d21e1782e9ff3cd473af261d980d
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-08 14:50:34 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,9 +2,9 @@
 
 $LANG = array();
 
-// Translation of file modules/disqus/config/module.php
-
-$LANG['Enter your disqus code'] = 'Enter your disqus code';
 // Translation of file modules/disqus/install.inc
 
 $LANG['Disqus'] = 'Disqus';
+// Translation of file modules/disqus/config/module.php
+
+$LANG['Enter your disqus code'] = 'Enter your disqus code';
